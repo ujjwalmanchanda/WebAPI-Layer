8c070d4ff59b1b287e09c341631978b10369070f
ujjwalmanchanda <ujjwalmanchanda5@gail.com>
2018-10-24 04:41:53 +0530

    Sign Up API

---
+++ WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class AccountController : ApiController
+    {
+        [Route("api/User/Register")]
+        [HttpPost]
+        public IdentityResult Register(AccountModel model)
+        {
+            var userStore = new UserStore<ApplicationUser>(new ApplicationDBContext());
+            var manager = new UserManager<ApplicationUser>(userStore);
+            var user = new ApplicationUser() { UserName = model.UserName, Email = model.Email };
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+
+            IdentityResult result = manager.Create(user, model.Password);
+            return result;
+        }
+    }
+}
---
+++ WebAPI/Models/IdentityModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WebAPI.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    
+    public class ApplicationDBContext: IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDBContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //AspNetUsers to Users
+            modelBuilder.Entity<ApplicationUser>().ToTable("User");
+
+            //IdentityRole to Role
+            modelBuilder.Entity<IdentityRole>().ToTable("Role");
+
+            //IdentityUserRole to UserRole
+            modelBuilder.Entity<IdentityUserRole>().ToTable("UserRole");
+
+            //IdentityUserClaim to UserClaims
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("UserClaims");
+
+            //IdentityUserLogin to UserLogin
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("UserLogin");
+        }
+
+    }
+}
